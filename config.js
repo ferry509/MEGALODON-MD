d1d44cddf5de74d6ad04ee99301af02bf32131e2
ferry509 <ernstalsaint71@gmail.com>
2025-05-17 16:45:54 -0500

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "MEGALODON~MD~PApWjaBb#_q62F4pthavWicroM8u94LE18_zRfgJMEK7c7YktW58",
+SESSION_ID: process.env.SESSION_ID || "MEGALODON~MD~TZhASRTI#APmhsJDE8DSJZj7bntdTuTENIPsRgnYIKKAYX3ZEHgM",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -52,7 +52,7 @@ LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar *MEGALODON-MD*⚡",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
-AUTO_REACT: process.env.AUTO_REACT || "false",
+AUTO_REACT: process.env.AUTO_REACT || "true",
 // make this true or false for auto react on all msgs
 ANTI_BAD: process.env.ANTI_BAD || "false",
 // false or true for anti bad words  
